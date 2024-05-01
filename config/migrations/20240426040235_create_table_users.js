bc402aed86893b74232a9777fab12deda6b46ca9
Lebackrobot <lebackman18@gmail.com>
2024-05-01 19:50:36 -0300

    🗃️ feat(user): migration

--- config/migrations/20240426040235_create_table_users.js
+++ config/migrations/20240426040235_create_table_users.js
@@ -11,7 +11,7 @@ export const up = function (knex) {
         table.string('email').notNullable().unique()
         table.binary('avatar')
         table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
-        table.timestamp('updated_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
     })
 }
 
