72655e2dd1b11f081b915056d9d123a2b2150107
Lebackrobot <lebackman18@gmail.com>
2024-05-01 19:51:42 -0300

    🗃️ feat(chat): chat migration

---
+++ config/migrations/20240501213325_create_chat_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function(knex) {
+  return knex.schema.createTable('chats', function (table) {
+    table.increments('id').primary(),
+    table.string('visibility', ['PUBLIC', 'PRIVATE']).notNullable().unique()
+    table.string('title').notNullable()
+    table.string('description').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function(knex) {
+    return knex.schema.dropTable('chats')
+};
